1163a5d73127a3d746882829cfb463933e96e5ba
proofit404 <proofit404@gmail.com>
2012-09-13 15:13:25 -0400

    Generic `list-action-on-graph' function.

--- src/etl_proxy/graph.clj
+++ src/etl_proxy/graph.clj
@@ -1,24 +1,39 @@
 ;; Copyright (c) 2012  Malyshev Artem  {-proofit404@gmail.com-}
 
 (ns etl-proxy.graph
-  "# Graph processing module."
   (:use [clojure.set]))
 
+;; ## Graph processing module.
+;;
 ;; The main aim of this module is definition of graph data structure and basic functions above
-;; it. Any graph in this program will defined as tuple of nodes set and arcs set. Situation in which
-;; some arc bind one or both nodes that doesn't members of nodes set will be considered as error.
+;; it. Any graph in this program will defined as tuple of vertices set and edges set. Situation in which
+;; some edge bind one or both vertices that doesn't members of vertices set will be considered as error.
 ;;
 ;; Basic module data structure:
 ;;
-;; - graph := [#{node} #{arc}]
-;; - node  := [id body]
-;; - arc   := [id id]
+;; - graph := [#{vertex} #{edge}]
+;; - vertex  := [id body]
+;; - edge   := [id id]
+;; - route := #{edge}
 ;; - id - unique natural number which must be reevaluated in every graph transformation.
 ;; - body - arbitrary data type which can express necessary abstract data.
-;; - route := #{arc}
 ;;
 
 (def empty-graph [#{} #{}])
+
+;; All graph modification must accomplish with functions which accept one processed item and
+;; intended graph. All of this functions return new modified graph as result. If you have some sort
+;; of such function and you need to process list of items, then you can use genetic definition
+;; below. It is a simple way to recursive process graph.
+
+(defn list-action-on-graph
+  "Create new graph comprising all new items from list processed by fun."
+  [fun]
+  (fn [item-list graph]
+    (if (empty? item-list)
+      graph
+      (recur (rest item-list)
+             (fun (first item-list) graph)))))
 
 ;; ## CRUD.
 ;; 
@@ -30,31 +45,31 @@
 ;; Better way to do this is write your own methods for converting exist data structures into our
 ;; graph representation. I think multimethods is a best way to implement such kind of parsers. For
 ;; example, if you have some tree-like data structure, then method accepted list of fields must
-;; create single node with body which will useful for you. If method accept non-terminal element of
-;; your tree, for example, list with nested lists, then you must create parent node that contain all
-;; data and children's nodes that contain nested lists. When you call your multimethod recursively it
+;; create single vertex with body which will useful for you. If method accept non-terminal element of
+;; your tree, for example, list with nested lists, then you must create parent vertex that contain all
+;; data and children's vertices that contain nested lists. When you call your multimethod recursively it
 ;; will parse tree-like data structure into graph until its leafs become terminal elements.
 ;;
 ;; Default way we do things above is accept single ISeq data structure and process it in below manner.
 ;;
-;; If we accept list without nested lists, then we create graph with node from its elements. If we
-;; accept list which contain nested sequences then all non-list elements is a body of root node and
-;; all list elements is child's nodes.
+;; If we accept list without nested lists, then we create graph with vertex from its elements. If we
+;; accept list which contain nested sequences then all non-list elements is a body of root vertex and
+;; all list elements is child's vertices.
 
 ;; ## Read section.
 
 (defn ids
   "Create list of all graph indices."
-  [[nodes arcs]]
-  (set (map first nodes)))
+  [[vertices edges]]
+  (set (map first vertices)))
 
 (defn bodies
-  "Create nodes contents list with arcs set lost."
-  [[nodes arcs]]
-  (set (map second nodes)))
+  "Create vertices contents list with edges set lost."
+  [[vertices edges]]
+  (set (map second vertices)))
 
 (defn available-id
-  "Return single id which isn't among to any node."
+  "Return single id which isn't among to any vertex."
   [graph]
   (inc
    (reduce max
@@ -62,101 +77,98 @@
            (conj (ids graph) 0))))
 
 (defn graph-member?
-  "If body belong to any node in the graph, then function return true."
+  "If body belong to any vertex in the graph, then function return true."
   [body graph]
   (contains? (bodies graph) body))
 
-(defn node-by-id
-  "Find node by her id field. Return its as vector of id and body."
-  [id [nodes arcs]]
-  (first (select (fn [[node-id node-body]] (= id node-id)) nodes)))
+(defn vertex-by-id
+  "Find vertex by her id field. Return its as vector of id and body."
+  [id [vertices edges]]
+  (first (select (fn [[vertex-id vertex-body]] (= id vertex-id)) vertices)))
 
 (defn body-by-id
-  "Find node by her id field. Return its body as is."
+  "Find vertex by her id field. Return its body as is."
   [id graph]
-  ;; Get second element (node body) in first node in yield set.
-  (second (node-by-id id graph)))
+  ;; Get second element (vertex body) in first vertex in yield set.
+  (second (vertex-by-id id graph)))
 
 (defn id-by-body
-  "Find node index by it content."
-  [body [nodes arcs]]
-  ;; Get first element (node id) in first node in yield set.
-  (first (first (select (fn [[node-id node-body]] (= body node-body)) nodes))))
+  "Find vertex index by it content."
+  [body [vertices edges]]
+  ;; Get first element (vertex id) in first vertex in yield set.
+  (first (first (select (fn [[vertex-id vertex-body]] (= body vertex-body)) vertices))))
 
 (defn relation-childs
-  "Return all nodes id with which current node is a parent (its childs)."
-  [id [nodes arcs]]
-  (map second (filter (fn [[parent child]] (= id parent)) arcs)))
+  "Return all vertices id with which current vertex is a parent (its childs)."
+  [id [vertices edges]]
+  (map second (filter (fn [[parent child]] (= id parent)) edges)))
 
 (defn relation-parents
-  "Return all nodes id with which current node is a child (its parents)."
-  [id [nodes arcs]]
-  (map first (filter (fn [[parent child]] (= id child)) arcs)))
+  "Return all vertices id with which current vertex is a child (its parents)."
+  [id [vertices edges]]
+  (map first (filter (fn [[parent child]] (= id child)) edges)))
+
+(defn edges-subset
+  "Return set of edges childs and parents correspond accepted two list."
+  [parents-list childs-list [vertices edges]]
+  (select (fn [[parent child]]
+            (and (not= (list) (filter #(= parent %) parents-list))
+                 (not= (list) (filter #(= child %) childs-list))))
+          edges))
 
 ;; ## Update section.
 ;;
 ;; Graph modification functions is self indexable function. You don't have to fix graph indices your
-;; self. If you will try to add node which already exist in current graph or to add arcs between
-;; nonexistent nodes, then resultant graph doesn't change at all.
+;; self. If you will try to add vertex which already exist in current graph or to add edges between
+;; nonexistent vertices, then resultant graph doesn't change at all.
 
-(defn add-node
-  "Create new graph with specified node."
+(defn add-vertex
+  "Create new graph with specified vertex."
   [body graph]
   (if-not (graph-member? body graph)
-    ((fn [[nodes arcs]]
-       (vector (conj nodes
-                     ;; This is new unique node for graph.
+    ((fn [[vertices edges]]
+       (vector (conj vertices
+                     ;; This is new unique vertex for graph.
                      (vector (available-id graph) body))
-               arcs))
-     ;; Apply conjunction of new node to graph.
+               edges))
+     ;; Apply conjunction of new vertex to graph.
      graph)
     ;; Return graph as is.
     graph))
 
-(defn add-arc
-  "Create new graph with specified arc."
-  [arc graph]
-  (if ;; Check that both ids point to existent nodes.
-      (subset? (set arc) (ids graph))
-    ((fn [[nodes arcs]]
-       (vector nodes
-               (conj arcs arc)))
-     ;; Apply conjunction of new arc to graph.
+(def add-vertices-list (list-action-on-graph add-vertex))
+
+(defn add-edge
+  "Create new graph with specified edge."
+  [edge graph]
+  (if ;; Check that both ids point to existent vertices.
+      (subset? (set edge) (ids graph))
+    ((fn [[vertices edges]]
+       (vector vertices
+               (conj edges edge)))
+     ;; Apply conjunction of new edge to graph.
      graph)
     ;; Return graph as is.
     graph))
+
+(def add-edges-list (list-action-on-graph add-edge))
 
 (defn add-parent
-  "Add given node as parent to id in graph. If node already exist, then simple add arc (bind nodes)."
+  "Add given vertex as parent to id in graph. If vertex already exist, then simple add edge (bind vertices)."
   [id-child body graph]
-  (let [new-graph (add-node body graph)]
-    (add-arc (vector (id-by-body body new-graph) id-child)
+  (let [new-graph (add-vertex body graph)]
+    (add-edge (vector (id-by-body body new-graph) id-child)
              new-graph)))
 
 (defn add-child
-  "Add given node as child to id in graph. If node already exist, then simple add arc (bind nodes)."
+  "Add given vertex as child to id in graph. If vertex already exist, then simple add edge (bind vertices)."
   [id-parent body graph]
-  (let [new-graph (add-node body graph)]
-    (add-arc (vector id-parent (id-by-body body new-graph))
+  (let [new-graph (add-vertex body graph)]
+    (add-edge (vector id-parent (id-by-body body new-graph))
              new-graph)))
 
-;; ### Actions on list of items with graph.
-
-(defn list-add-action
-  "Create new graph comprising all new items from list processed by fun."
-  [fun]
-  (fn [item-list graph]
-    (if (empty? item-list)
-      graph
-      (recur (rest item-list)
-             (fun (first item-list) graph)))))
-
-(def add-nodes-list (list-add-action add-node))
-
-(def add-arcs-list (list-add-action add-arc))
-
 (defn add-child-list
-  "Create new graph in which all passed nodes will be childs of passed id."
+  "Create new graph in which all passed vertices will be childs of passed id."
   [id-parent item-list graph]
   (if (empty? item-list)
       graph
@@ -168,57 +180,53 @@
 
 ;; ## Destroy section.
 
-(defn delete-arc
-  "Delete arc (bind) between existing nodes."
-  [arc [nodes arcs]]
-  (vector nodes (disj arcs arc)))
-
-;; Main difference between tie and delete functions is in bind processing after node remove. For
-;; example, we have three bound object A -> B -> C with parent -> child relation type and we need
-;; remove B node from graph. Tie function will add A -> C relation into graph and delete function will
-;; not. With delete function we will lost relation between A and C objects.
-
-(defn delete-node
-  "Delete node from graph, clear its arcs."
+(defn delete-vertex
+  "Delete vertex from graph, clear its edges."
   [id graph]
-  ((fn [[nodes arcs]]
+  ((fn [[vertices edges]]
      (vector
-      (disj nodes (node-by-id id graph))
-      ;; Arcs set without any object parent or child relations.
-      (difference arcs (select (fn [[parent child]] (or (= parent id) (= child id))) arcs))))
+      (disj vertices (vertex-by-id id graph))
+      ;; Edges set without any object parent or child relations.
+      (difference edges (select (fn [[parent child]] (or (= parent id) (= child id))) edges))))
    graph))
 
-(defn delete-nodes-list
-  "Delete all nodes from delivered list and remove those arcs."
-  [id-list graph]
-  (if (empty? id-list)
-    graph
-    (recur (rest id-list)
-           (delete-node (first id-list) graph))))
+(def delete-vertices-list (list-action-on-graph delete-vertex))
 
-(defn tie-node
-  "Delete node from graph, clear its arcs and create arcs between its parents and childs for saving
+(defn delete-edge
+  "Delete edge (bind) between existing vertices."
+  [edge [vertices edges]]
+  (vector vertices (disj edges edge)))
+
+;; Main difference between tie and delete functions is in bind processing after vertex remove. For
+;; example, we have three bound object A -> B -> C with parent -> child relation type and we need
+;; remove B vertex from graph. Tie function will add A -> C relation into graph and delete function will
+;; not. With delete function we will lost relation between A and C objects.
+
+(defn tie-vertex
+  "Delete vertex from graph, clear its edges and create edges between its parents and childs for saving
   object relations without intermediate object."
   [id graph]
-  ;; Create list of arcs as direct multiply parent with child.
-  (let [restore-arcs (mapcat
+  ;; Create list of edges as direct multiply parent with child.
+  (let [restore-edges (mapcat
                       (fn [parent]
                         (map #(vector parent %)
                              (relation-childs id graph)))
                       (relation-parents id graph))]
-    (delete-node id (add-arcs-list restore-arcs graph))))
+    (delete-vertex id (add-edges-list restore-edges graph))))
+
+(def tie-vertices-list (list-action-on-graph tie-vertex))
 
 ;; Delete sub tree function can be used only with _LIST_ of roots because when it recursively call
 ;; itself such situation become common. Function for one root mast just to wrap this item into one
 ;; element list and recall itself. You can write such function if you need it as much.
 
 (defn delete-sub-tree
-  "Accept node id list every item in which point to the root of sub tree. Then delete all nodes and
-  arcs belongs to those sub-tree."
+  "Accept vertex id list every item in which point to the root of sub tree. Then delete all vertices and
+  edges belongs to those sub-tree."
   [roots graph]
-  (let [;; Store graph without nodes accepted with roots.
-        current-level-map (delete-nodes-list roots graph)
-        ;; Store all child's from every roots node in the single list.
+  (let [;; Store graph without vertices accepted with roots.
+        current-level-map (delete-vertices-list roots graph)
+        ;; Store all child's from every roots vertex in the single list.
         lower-level-childs (mapcat #(relation-childs % graph) roots)]
     (if (empty? lower-level-childs)
       current-level-map
@@ -227,33 +235,33 @@
 (defn delete-self-loops
   "Return clear graph without relations between objects with it self (self loops). For example for
   object A remove A -> A relation."
-  [[nodes arcs]]
-  (vector nodes
+  [[vertices edges]]
+  (vector vertices
           (select (fn [[parent child]] (not= parent child))
-                  arcs)))
+                  edges)))
 
 ;; ## Graph route analyze tools.
 ;;
 ;; TODO: write documentation for route block.
 
 (defn tie-route
-  "Return route without arcs which contain accepted id as child or as parent. This function restore
+  "Return route without edges which contain accepted id as child or as parent. This function restore
   parent-child relations between remain objects without purged intermediate object. For example if
   we have relation below A -> B -> C and we tie B object then A -> C relation will be added."
-  [id arcs]
-  (let [restore-arcs
+  [id edges]
+  (let [restore-edges
         (mapcat
          (fn [child]
            ;; Restore parent-child relation.
            (map #(vector % child)
                 ;; Select all parents of tie id.
-                (map first (select (fn [[parent child]] (= child id)) arcs))))
+                (map first (select (fn [[parent child]] (= child id)) edges))))
          ;; Select all childs of tie id.
-         (map second (select (fn [[parent child]] (= parent id)) arcs)))]
+         (map second (select (fn [[parent child]] (= parent id)) edges)))]
     (difference
-     (union arcs restore-arcs)
-     ;; All arcs which contain id as parent or as child.
-     (select (fn [[parent child]] (or (= parent id) (= child id))) arcs))))
+     (union edges restore-edges)
+     ;; All edges which contain id as parent or as child.
+     (select (fn [[parent child]] (or (= parent id) (= child id))) edges))))
 
 (defn intermediate-id
   "Return any intermediate id from route except beginning and end points."
@@ -266,30 +274,27 @@
                   (set (map second route))))))
 
 (defn route?
-  "Return true if accepted arcs set is a route from A to B node."
-  [from to arcs]
+  "Return true if accepted edges set is a route from A to B vertex."
+  [from to edges]
   (cond
-   (= arcs #{[from to]}) true
-   (= arcs #{}) false
+   (= edges #{[from to]}) true
+   (= edges #{}) false
    :else (recur from
                 to
-                (tie-route (intermediate-id from to arcs) arcs))))
+                (tie-route (intermediate-id from to edges) edges))))
 
 (defn route-list
   "Return list of all possible routes from parent to child in graph."
-  [from to graph]
-  (let [routes (list)]
-    ;; FIXME: I don't work yet.
-    ))
+  [from to graph])
 
-(defn delete-obvious-arcs
-  "Return clear graph without obvious direct relations between objects. For example for node sequence
-  expressed through A -> B -> C and direct A -> C arcs last will deleted from graph."
+(defn delete-obvious-edges
+  "Return clear graph without obvious direct relations between objects. For example for vertex sequence
+  expressed through A -> B -> C and direct A -> C edges last will deleted from graph."
   [graph]
-  ((fn [[nodes arcs]]
-     (vector nodes
+  ((fn [[vertices edges]]
+     (vector vertices
              (select (fn [[parent child]]
                        (= (list [parent child])
                           (route-list parent child graph)))
-                     arcs)))
+                     edges)))
    graph))
--- src/etl_proxy/json.clj
+++ src/etl_proxy/json.clj
@@ -1,10 +1,11 @@
 ;; Copyright (c) 2012  Malyshev Artem  {-proofit404@gmail.com-}
 
 (ns etl-proxy.json
-  "# Convert JSON markup language into graph data structure."
   (:use [etl-proxy.graph]
         [cheshire.core]))
 
+;; ## Convert JSON markup language into graph data structure.
+;;
 ;; This module was written for possibility of creation graph structure from json markup language
 ;; document. Internal mechanics of service require to represent any data structure as graph in set
 ;; terms. External communication has provided by JSON markup language and we need to transform it into
@@ -12,46 +13,46 @@
 ;;
 ;; With `cheshire` library clojure can represent json format as map composed of strings, vectors and
 ;; other maps. Because maps keys used as structure representation facility we can just to use values
-;; in it. All dependencies in graph represented with arcs set and keys safety doesn't necessary.
+;; in it. All dependencies in graph represented with edges set and keys safety doesn't necessary.
 ;;
-;; Any node in graph can contain in its body terminal or non-terminal part of json document
-;; structure. If node is non-terminal, then it mast be simplified.
+;; Any vertex in graph can contain in its body terminal or non-terminal part of json document
+;; structure. If vertex is non-terminal, then it mast be simplified.
 
-;; ## Node simplification principle.
+;; ## Vertex simplification principle.
 ;;
-;; I chose next rules for simplify nodes in graph:
+;; I chose next rules for simplify vertices in graph:
 ;;
-;; - non-sequences and lists will suppose as simplified nodes.
-;; - any map will suppose as single nested node and it will transform to single list without maps
+;; - non-sequences and lists will suppose as simplified vertices.
+;; - any map will suppose as single nested vertex and it will transform to single list without maps
 ;;   keys (only values).
-;; - any vector will suppose as vector of nested nodes and its each element will process in distinct
-;;   single node manner.
+;; - any vector will suppose as vector of nested vertices and its each element will process in distinct
+;;   single vertex manner.
 ;;
 
-(defn simple-node?
-  "Return true if accepted node doesn't contain nested nodes. For simplification rules visit project
+(defn simple-vertex?
+  "Return true if accepted vertex doesn't contain nested vertices. For simplification rules visit project
   documentation."
   [body]
   (not (or (vector? body)
            (map? body))))
 
-(defn simplify-node
-  "Return single simplified parent node. For simplification rules visit project documentation."
+(defn simplify-vertex
+  "Return single simplified parent vertex. For simplification rules visit project documentation."
   [body]
-  (if (simple-node? body)
+  (if (simple-vertex? body)
     body
     ;; Procession vector variant here is ignored because it will
-    ;; solve in the rest of simplified node and doesn't occur here.
-    (filter simple-node?
+    ;; solve in the rest of simplified vertex and doesn't occur here.
+    (filter simple-vertex?
             (vals body))))
 
-(defn rest-of-node
-  "Return list of non simplified child nodes. For simplification rules visit project documentation."
+(defn rest-of-vertex
+  "Return list of non simplified child vertices. For simplification rules visit project documentation."
   [body]
   ;; At this point we can accept only map because vectors
   ;; process element by element and doesn't return after
   ;; call this function.
-  (if (simple-node? body)
+  (if (simple-vertex? body)
     (list)
     (concat
      ;; All vector elements conjuction into single list. 
@@ -64,56 +65,56 @@
 
 ;; ## JSON processing principle.
 ;;
-;; The simplest approach to recursive processing nested sequences is create graph with one node
-;; without any arcs in it. Then we need to simplify this node into one parent node and list of its
-;; children nodes. Now we can create new graph without first node but with new nodes generated
-;; above. Now we have graph contained same nodes and its arcs which demonstrates nodes parent-child
-;; relations. We reproduce our actions with current graph nodes until we have node set which we
+;; The simplest approach to recursive processing nested sequences is create graph with one vertex
+;; without any edges in it. Then we need to simplify this vertex into one parent vertex and list of its
+;; children vertices. Now we can create new graph without first vertex but with new vertices generated
+;; above. Now we have graph contained same vertices and its edges which demonstrates vertices parent-child
+;; relations. We reproduce our actions with current graph vertices until we have vertex set which we
 ;; can't simplify anymore.
 ;;
-;; If we will process node set of graph in manner of sequence, than we can supply for each node its
+;; If we will process vertex set of graph in manner of sequence, than we can supply for each vertex its
 ;; simplified version and list of not simplified parts which was derived during simplification of
-;; parent node. And all what we need now is substitution simplified node into not simplified and
-;; then add not simplified childs to the new node.
+;; parent vertex. And all what we need now is substitution simplified vertex into not simplified and
+;; then add not simplified childs to the new vertex.
 ;;
-;; At this point we have problem we can't change body of any node without automatic changing its id.
-;; This behaviour result in lost arcs relation validity. I solve this problem in simple manner.
-;; Instead of changing body of existed node we will add its simplified version as child AND as
+;; At this point we have problem we can't change body of any vertex without automatic changing its id.
+;; This behaviour result in lost edges relation validity. I solve this problem in simple manner.
+;; Instead of changing body of existed vertex we will add its simplified version as child AND as
 ;; parent to it, then add non simplified childs to simplified parent and finally TIE non simplified
-;; node. Those action will result in automatic reevaluate arc indices in correct manner. When we tie
-;; non-simplified node new arcs appear excepting new node, because new simplified node fit as self
-;; parent and child to non-simplified. There is two kinds of parasite arcs. First is a parent-child
+;; vertex. Those action will result in automatic reevaluate edge indices in correct manner. When we tie
+;; non-simplified vertex new edges appear excepting new vertex, because new simplified vertex fit as self
+;; parent and child to non-simplified. There is two kinds of parasite edges. First is a parent-child
 ;; relation between A -> C objects when we already has A -> B -> C. So resulted graph mast be bring
 ;; to a light form of graph. Second is a self-loop relation A -> A object. Graph must be cleaned
 ;; from this relation type.
 
 (defn simple-graph?
-  "Return true if there is no non-terminal nodes in the graph."
+  "Return true if there is no non-terminal vertices in the graph."
   [graph]
   (= (list)
-     ((fn [[nodes arcs]]
-        ;; Return list of non-terminal nodes from graph.
-        (filter #(not (simple-node? %)) nodes))
+     ((fn [[vertices edges]]
+        ;; Return list of non-terminal vertices from graph.
+        (filter #(not (simple-vertex? %)) vertices))
       graph)))
 
 (defn grow-sub-tree
-  "Add simplified node and rest of it into graph and tie non-simplified node."
+  "Add simplified vertex and rest of it into graph and tie non-simplified vertex."
   [id graph]
   (let [body (body-by-id id graph)
-        simple-body (simplify-node body)
-        ;; Add simplified node as parent and child to the non-simplified node.
+        simple-body (simplify-vertex body)
+        ;; Add simplified vertex as parent and child to the non-simplified vertex.
         new-graph (add-child id
                              simple-body
                              (add-parent id
                                          simple-body
                                          graph))
         simple-id (id-by-body simple-body new-graph)]
-    (if-not (simple-node? body)
-      (delete-obvious-arcs
+    (if-not (simple-vertex? body)
+      (delete-obvious-edges
        (delete-self-loops
-        (tie-node id                                  ;; Old non-simplified node.
-                  (add-child-list simple-id           ;; New simplified node.
-                                  (rest-of-node body) ;; It's non-simplified childs.
+        (tie-vertex id                                  ;; Old non-simplified vertex.
+                  (add-child-list simple-id           ;; New simplified vertex.
+                                  (rest-of-vertex body) ;; It's non-simplified childs.
                                   new-graph))))
       graph)))
 
@@ -126,7 +127,7 @@
   "Accept json hash map and recursively convert it into graph."
   [json-map]
   (simplify-graph
-   (add-node json-map empty-graph)))
+   (add-vertex json-map empty-graph)))
 
 ;; USEME:
 ;; <pre><code>(parse-string (slurp "example.json") true)
